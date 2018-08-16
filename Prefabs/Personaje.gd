cd3c572c1c97ff439ade573008836737117e0c0c
MauriBian <mauricioosvaldobianchi@gmail.com>
2018-08-16 19:41:06 -0300

    fixSalto

--- Prefabs/Personaje.gd
+++ Prefabs/Personaje.gd
@@ -1,26 +1,46 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+export var gravedad = 5
+var salto = 0
+var collision
+
+
+
 
 func _ready():
-	pass
+ pass
 
 
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-func saltar(saltoExtra = 0, salto = 0):
-	apply_impulse(Vector2(0,0),Vector2(0,salto + saltoExtra))
-	
-	
-	
+func _process(delta):
+	caerYSaltar()
+	dejaDeSubir(delta)
 	
 
+func caerYSaltar():
+	collision = move_and_collide(Vector2(0,gravedad - salto))
+	if collision != null:
+		salto = collision.collider.colision()
+		print(gravedad - salto)
+		
+		
+		
+		
+
+	
+	
+func dejaDeSubir(deltis):
+	if salto >0 :
+		salto -= (gravedad + salto)/2 * deltis
+	
+
+		
+
+	
 
 
 
-			
+	
+
--- Prefabs/resorte.gd
+++ Prefabs/resorte.gd
@@ -3,19 +3,21 @@ extends RigidBody2D
 # class member variables go here, for example:
 # var a = 2
 
-export var salto = -200
-export var saltoExtra = -300
+export var salto = 0
+export var saltoExtra = 0
+export var velocidadCaida = 0
 func _ready():
-	set_contact_monitor( true )
-	set_max_contacts_reported( 5 )
-	connect("body_entered",self,"colision")
-
+	#set_contact_monitor( true )
+	#set_max_contacts_reported( 5 )
+	#connect("body_entered",self,"colision")
+	pass
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-func colision(who):
-	print(self.get_name()," is colliding with ",who.get_name())
-	if who.get_name() == "Personaje":
-		who.saltar(salto,saltoExtra)
+func colision():
+	return salto + saltoExtra
+
+func velocidadCaida():
+	return velocidadCaida
 	
